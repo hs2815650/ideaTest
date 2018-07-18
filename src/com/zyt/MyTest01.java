a28c7f49e86c82858d85b3ce37edb8f6a24a48f5
zhangyongting <846422864@163.com>
2018-07-18 19:20:29 +0800

    first commit

---
+++ src/com/zyt/MyTest01.java
@@ -0,0 +1,9 @@
+package com.zyt;
+
+public class MyTest01 {
+    public static void main(String[] args) {
+        for(int i =0;i<10;i++) {
+            System.out.println("hello wangchao"+i);
+        }
+    }
+}
